5d42f793656c2372d7e2458a8ad6206815fb400c
GesusOfYou <65038622+GesusOfYou@users.noreply.github.com>
2020-05-08 21:08:24 +0200

    test stuff and so

--- test.py
+++ test.py
@@ -1,1 +1,33 @@
-import pandas as pd
+# -*- coding: utf-8 -*-
+
+# Sample Python code for youtube.commentThreads.list
+# See instructions for running these code samples locally:
+# https://developers.google.com/explorer-help/guides/code_samples#python
+
+import os
+
+import googleapiclient.discovery
+
+def main():
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "0"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    DEVELOPER_KEY = "AIzaSyDzmGwXqhtWb8ankH-4PndjIn1erS18aZo"
+
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey = DEVELOPER_KEY)
+
+    request = youtube.commentThreads().list(
+        part="snippet,replies",
+        maxResults=51,
+        videoId="6Oob572SYns"
+    )
+    response = request.execute()
+
+    print(response)
+
+if __name__ == "__main__":
+    main()
